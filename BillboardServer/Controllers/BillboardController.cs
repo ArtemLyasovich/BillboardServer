0b88e25ec626ae2d37793a0461fd6b1058ed5081
Artsemi Liasovich <artemlysovich@gmail.com>
2024-06-21 23:21:22 +0300

    Clear project

---
+++ BillboardServer/Controllers/BillboardController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BillboardServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BillboardController : ControllerBase
+    {
+        
+    }
+}
