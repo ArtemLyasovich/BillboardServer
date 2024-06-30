246192c2f2cc3e00efdc84ed6e30d17ae5fb699a
Artsemi Liasovich <artemlysovich@gmail.com>
2024-06-30 23:06:23 +0300

    Implement message controller

--- BillboardServer/Controllers/BillboardController.cs
+++ BillboardServer/Controllers/BillboardController.cs
@@ -1,3 +1,4 @@
+using BillboardServer.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BillboardServer.Controllers
@@ -6,6 +7,30 @@ namespace BillboardServer.Controllers
     [Route("[controller]")]
     public class BillboardController : ControllerBase
     {
-        
+        private readonly MessageQueueService _messageQueueService;
+
+        public BillboardController(MessageQueueService messageQueueService)
+        {
+            _messageQueueService = messageQueueService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCurrentMessage()
+        {
+            var message = _messageQueueService.GetCurrentMessage();
+            return Ok(message);
+        }
+
+        [HttpPost]
+        public IActionResult PostMessage([FromBody] string message)
+        {
+            if (string.IsNullOrWhiteSpace(message))
+            {
+                return BadRequest("Message cannot be empty.");
+            }
+
+            _messageQueueService.EnqueueMessage(message);
+            return Ok();
+        }
     }
 }
