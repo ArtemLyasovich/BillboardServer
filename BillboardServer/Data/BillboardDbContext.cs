08b5442c9b0a775c2d9bc5d514a708c898368802
Artsemi Liasovich <artemlysovich@gmail.com>
2024-08-15 00:49:25 +0300

    Initial database setup with Message model and BillboardDbContext

---
+++ BillboardServer/Data/BillboardDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BillboardServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BillboardServer.Data;
+
+public class BillboardDbContext : DbContext
+{
+    public DbSet<Message> Messages { get; set; }
+
+    public BillboardDbContext(DbContextOptions<BillboardDbContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Message>()
+            .HasKey(m => m.Id);
+
+        modelBuilder.Entity<Message>()
+            .Property(m => m.Content)
+            .IsRequired();
+
+        modelBuilder.Entity<Message>()
+            .Property(m => m.CreatedAt)
+            .IsRequired();
+    }
+}
---
+++ BillboardServer/Models/Message.cs
@@ -0,0 +1,8 @@
+﻿namespace BillboardServer.Models;
+
+public class Message
+{
+    public int Id { get; set; }
+    public string Content { get; set; } = string.Empty;
+    public DateTime CreatedAt { get; set; }
+}
