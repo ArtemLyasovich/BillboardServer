0e3c311d5b2a3fb06a27df37053a4d8cb9f5a652
Artsemi Liasovich <artemlysovich@gmail.com>
2024-08-15 00:50:00 +0300

    Implement BillboardRepository for database operations

---
+++ BillboardServer/Repositories/BillboardRepository.cs
@@ -0,0 +1,34 @@
+﻿using BillboardServer.Data;
+using BillboardServer.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace BillboardServer.Repositories
+{
+    public class BillboardRepository
+    {
+        private readonly BillboardDbContext _context;
+
+        public BillboardRepository(BillboardDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Message>> GetAllMessagesAsync()
+        {
+            var allMessages = await _context.Messages.OrderBy(m => m.CreatedAt).ToListAsync();
+
+            _context.Messages.RemoveRange(allMessages);
+            await _context.SaveChangesAsync();
+
+            return allMessages;
+        }
+
+
+        public async Task AddMessagesAsync(IEnumerable<Message> messages)
+        {
+            await _context.Messages.AddRangeAsync(messages);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
