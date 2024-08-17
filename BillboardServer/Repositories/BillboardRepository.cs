911b14dac25adf4af6e6e47263b4834aedd47e57
Artsemi Liasovich <artemlysovich@gmail.com>
2024-08-17 21:01:28 +0300

    Add logging to key components of the application

--- BillboardServer/Repositories/BillboardRepository.cs
+++ BillboardServer/Repositories/BillboardRepository.cs
@@ -4,15 +4,19 @@ using BillboardServer.Extensions;
 
 using Microsoft.EntityFrameworkCore;
 
+using static BillboardServer.Consts.Consts;
+
 namespace BillboardServer.Repositories;
 
 public class BillboardRepository
 {
     private readonly BillboardDbContext _context;
+    private readonly ILogger<BillboardRepository> _logger;
 
-    public BillboardRepository(BillboardDbContext context)
+    public BillboardRepository(BillboardDbContext context, ILogger<BillboardRepository> logger)
     {
         _context = context;
+        _logger = logger;
     }
 
     public async Task<IEnumerable<Message>> GetAllMessagesAsync()
@@ -23,9 +27,15 @@ public class BillboardRepository
 
         if (allMessages.Any())
         {
+            _logger.LogInformation("{0} - Loaded {1} messages from the database.", REPOSITORY_LOGID, allMessages.Count);
+
             allMessages.NormalizeDates();
             _context.Messages.RemoveRange(allMessages);
             await _context.SaveChangesAsync();
+        }
+        else
+        {
+            _logger.LogInformation("{0} - No messages found in the database to load.", REPOSITORY_LOGID);
         }
 
         return allMessages;
@@ -35,9 +45,17 @@ public class BillboardRepository
     {
         if (messages.Any())
         {
+            _logger.LogInformation("{0} - Adding {1} messages to the database.", REPOSITORY_LOGID, messages.Count());
+
             messages.NormalizeDates();
             _context.Messages.AddRange(messages);
             await _context.SaveChangesAsync();
+
+            _logger.LogInformation("{0} - Successfully added {1} messages to the database.", REPOSITORY_LOGID, messages.Count());
+        }
+        else
+        {
+            _logger.LogInformation("{0} - No messages to add to the database.", REPOSITORY_LOGID);
         }
     }
 }
